c46f607e366d60dd2c039ca47cdc83ce73066320
Rob Wood <rdrwood@gmail.com>
2018-08-14 10:59:02 -0700

    made cookie

--- CookieSample/Controllers/HomeController.cs
+++ CookieSample/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CookieSample.Models;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,13 +12,42 @@ namespace CookieSample.Controllers
     {
         public ActionResult Index()
         {
+            var prod1 = new Product()
+            {
+                ProductID = 100,
+                Price = 9.99,
+                Quantity = 2
+            };
+
+            var prod2 = new Product()
+            {
+                ProductID = 130,
+                Price = 20,
+                Quantity = 1
+            };
+
+            List<Product> products = new List<Product>() { prod1, prod2 };
+            // create a cookie
+            var cookie = new HttpCookie("Test");
+            // cookie.Value = "Rob Was here";
+            // convert all products to a json string
+            cookie.Value = JsonConvert.SerializeObject(products);
+            cookie.Expires = DateTime.Now.AddDays(7);
+            // sends the cookie to the user
+            Response.Cookies.Add(cookie);
+
             return View();
         }
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
+            // read cookie data
+            if(Request.Cookies["Test"] != null)
+            {
+                string data = Request.Cookies["Test"].Value;
+                List<Product> prods = JsonConvert.DeserializeObject<List<Product>>(data);
+                return View(prods);
+            }
             return View();
         }
 
---
+++ CookieSample/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CookieSample.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public short Quantity { get; set; }
+        public double Price { get; set; }
+    }
+}
